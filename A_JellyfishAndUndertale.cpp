1d27a5184e03a6190258968a431148944cd5f7f8
Devarsh Makwana <devarshppsv@gmail.com>
2023-10-01 18:19:16 +0530

    Completed 2 problems

---
+++ A_JellyfishAndUndertale.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    auto name = "Hello this is Devarsh";
+    std::cout << name << std::endl;
+    std::cout << &name << std::endl;
+    return 0;
+}
---
+++ A_JellyfishAndUndertale.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())):
+    a,b,n = list(map(int,input().split(" ")))
+    if(a > 0 and b > 0 and n > 0 and n<=100):
+        arr = sorted(list(map(int,input().split(" "))))
+        res = sum(list(map(lambda x: a-1 if x>a else x,arr))) + b 
+        print(res)

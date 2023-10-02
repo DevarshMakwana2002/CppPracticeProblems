852c5fdd6071adb8a791592d67afbf4cecf15b44
Devarsh Makwana <devarshppsv@gmail.com>
2023-10-02 13:09:50 +0530

    solved in cpp A_JellyfishAndUndertale

--- A_JellyfishAndUndertale.cpp
+++ A_JellyfishAndUndertale.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
-int main(){
-    auto name = "Hello this is Devarsh";
-    std::cout << name << std::endl;
-    std::cout << &name << std::endl;
+using namespace std;
+
+void solve()
+{
+    int a, b, n;
+    cin >> a >> b >> n;
+    int arr[n];
+    long long int sum = b;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+        arr[i] = min(arr[i], a - 1);
+        sum += arr[i];
+    }
+    cout << sum << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t != 0)
+    {
+        solve();
+        t--;
+    }
     return 0;
 }

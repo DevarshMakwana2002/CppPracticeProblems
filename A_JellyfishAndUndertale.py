c9ff0a5cb2ac87bde6b9ef2d8cc524b3ebd12690
Devarsh Makwana <devarshppsv@gmail.com>
2023-10-02 12:39:43 +0530

    Accepted Python A_JellyfishAndUndertale

--- A_JellyfishAndUndertale.py
+++ A_JellyfishAndUndertale.py
@@ -2,5 +2,5 @@ for _ in range(int(input())):
     a,b,n = list(map(int,input().split(" ")))
     if(a > 0 and b > 0 and n > 0 and n<=100):
         arr = sorted(list(map(int,input().split(" "))))
-        res = sum(list(map(lambda x: a-1 if x>a else x,arr))) + b 
+        res = sum(list(map(lambda x: a-1 if x>a-1 else x,arr))) + b 
         print(res)

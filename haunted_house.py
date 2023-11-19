31d50e9dd2d5aefc037dc1d0bbe1a312f585595d
DevarshMakwana2002 <devershppsv@gmail.com>
2023-11-19 17:12:57 +0530

    haunted_house rewriting the logic

--- haunted_house.py
+++ haunted_house.py
@@ -1,15 +1,4 @@
-def findmin(i,stri):
-    if stri[i-1] == 1:
-        for j in range(i,len(stri)):
-            temp=stri[j-1]
-            stri[j-1]=stri[j]
-            stri[j]=temp
-            if stri[j-1] == 0:
-                return j
-    else: return -1
-
 for _ in range(int(input())):
-    x = int(input())
-    stri = list(map(int,reversed(str(input()))))
-    for i in range(1,x):
-        print(findmin(i,stri))
+    n = int(input())
+    lst = list(map(int,input().split(' ').reverse()))
+    print(lst)
